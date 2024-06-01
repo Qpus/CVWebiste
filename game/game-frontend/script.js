f111e12a14193b3a1d56ecf6b8e0466d2ca5f6a5
Aleksandar <aleksandargrubic@live.co.uk>
2024-06-01 12:24:24 +0200

    fix

--- game/game-frontend/script.js
+++ game/game-frontend/script.js
@@ -49,7 +49,7 @@ function startGame() {
 }
 
 function saveScore(name, score) {
-  fetch("https://ec2-3-208-27-143.compute-1.amazonaws.com:3000/save-score", {
+  fetch("ec2-35-173-128-174.compute-1.amazonaws.com:3000/save-score", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -72,7 +72,7 @@ function saveScore(name, score) {
 }
 
 function fetchScores() {
-  fetch("https://ec2-3-208-27-143.compute-1.amazonaws.com:3000/get-scores")
+  fetch("ec2-35-173-128-174.compute-1.amazonaws.com:3000/get-scores")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
